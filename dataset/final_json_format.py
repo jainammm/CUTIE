39f0eae3d54a02bb1f94681f727f828dd89d03b9
Dilip Sharma <dilipsharma640@gmail.com>
2020-07-27 23:50:57 +0530

    Add fields to json dataset.

--- dataset/final_json_format.py
+++ dataset/final_json_format.py
@@ -4,11 +4,14 @@ import os
 directory = 'Images'
 destination = 'Images'
 
-i=0
+fields = ['Seller State', 'Seller ID', 'Seller Name', 'Seller Address', 'Seller GSTIN Number',
+          'Country of Origin', 'Currency', 'Description', 'Invoice Number', 'Invoice Date', 'Due Date',
+          'Total Invoice amount entered by WH operator', 'Total Invoice Quantity entered by WH operator',
+          'Total TCS Collected', 'Round Off Charges', 'PO Number', 'Invoice Items Total Amount',
+          'Invoice Items total quantity', 'Buyer GSTIN Number', 'Ship to Address']
 
 for filename in os.listdir(directory):
-    if filename.endswith(".json") and i==0:
-        i+=1 
+    if filename.endswith(".json"):
         file_path = directory + '/' + filename
         image_path = filename[:-4] + 'png'
 
@@ -17,9 +20,20 @@ for filename in os.listdir(directory):
 
             data['fields'] = []
 
+            for f in fields:
+                data['fields'].append(
+                    {
+                        "field_name": f.upper().replace(' ', '_'),
+                        "value_id": [],
+                        "value_text": [],
+                        "key_id": [],
+                        "key_text": []
+                    }
+                )
+
             data['global_attributes'] = {
                 "file_id": image_path
             }
 
-        with open('jainam.json', 'w') as outfile:
+        with open(file_path, 'w') as outfile:
             json.dump(data, outfile)
